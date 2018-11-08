7e91ad2d6af41c1307b18ac8c17a35bb2803c3a1
valyadvorzhak <43156798+valyadvorzhak@users.noreply.github.com>
2018-11-08 13:39:45 +0200

    Add files via upload

---
+++ Lesson1/primer2.py
@@ -0,0 +1,16 @@
+#Ситанксис языка 3
+
+
+def valyala (print_all_text):
+    print(print_all_text)
+"""
+Пример
+Комментария
+
+""" 
+def main():
+    valyala("Хочу учить питон!!!")
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ Lesson1/project_zillya1.py
@@ -0,0 +1,9 @@
+#Проект. Создание зелья здоровья в игре
+
+import random
+
+heals=50 #присваиваем первоначальное знаение здоровью персонажа
+
+potion_health = random.randint(25, 50)
+print(potion_health)
+
